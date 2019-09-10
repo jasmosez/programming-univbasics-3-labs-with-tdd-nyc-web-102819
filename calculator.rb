42bf5d7e0b596c3d9f39a64ad4f4849c4064023c
James Schaffer <jas.schaffer@gmail.com>
2019-09-10 18:03:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 1
-second_number = 2
+second_number = 0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
